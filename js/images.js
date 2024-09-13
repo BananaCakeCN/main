be05dd7dc0cfecc37253c5acc715e2a95a09ad51
BananaCakeCN <103292046+BananaCakeCN@users.noreply.github.com>
2024-09-13 15:12:00 +0800

    Update images.js

--- js/images.js
+++ js/images.js
@@ -124,9 +124,32 @@ for (var i = 0;i < document.getElementsByClassName('photo').length; i++){
         await sleep(500)
         imageIsOpen = true
         document.querySelector('.photoViewer').style.cssText = 'height: 100%;width: 100%;background-color: #FFF;display: flex;'
-        document.querySelector('.photoViewer').innerHTML = '<img style="max-height: '+(document.documentElement.clientHeight-116)+'px;" src="'+this.childNodes[0].src+'" class="ViewerImage"></img><div class="imageTop"><p class="imageInfo">'+images[this.childNodes[0].src.split('/')[this.childNodes[0].src.split('/').length-1].split('.')[0]][1]+'</p><p class="imageInfo2">'+images[this.childNodes[0].src.split('/')[this.childNodes[0].src.split('/').length-1].split('.')[0]][0]+'</p><div class="button1 return imageReturn" onclick="closeImage()"><img class="return-img2" src="img/return.svg"></div></div>'
+        document.querySelector('.photoViewer').innerHTML = '<img style="max-height: '+(document.documentElement.clientHeight-116)+'px;" src="'+this.childNodes[0].src+'" class="ViewerImage"></img><div class="imageTop"><p class="imageInfo">'+images[this.childNodes[0].src.split('/')[this.childNodes[0].src.split('/').length-1].split('.')[0]][1]+'</p><p class="imageInfo2">'+images[this.childNodes[0].src.split('/')[this.childNodes[0].src.split('/').length-1].split('.')[0]][0]+'</p><div class="button1 return imageReturn" onclick="closeImage()"><img class="return-img2" src="img/return.svg"></div><div class="button1 imageDetails" onclick="imageDetails(`' + root + '`, ' + i + ')"><img class="details-img" src="img/info.circle.svg"></div></div><div id="imgDetails" style="width: 0; height: 0;"></div>'
         if(document.documentElement.clientWidth < document.documentElement.clientHeight){
             document.querySelector('.ViewerImage').style.cssText = 'max-height: '+(document.documentElement.clientHeight-166)+'px;bottom: 50px;'
         }
+    }
+}
+async function imageDetails(root, index){
+    if(document.getElementById('imgDetails').style.cssText == 'width: 0px; height: 0px;'){
+        document.getElementById('imgDetails').style.cssText = 'width: 300px; position: absolute; right: 13px; background-color: #fff; border: #e6e6e6 1px solid; border-radius: 7px; top: 113px; padding: 10px; color: #404040;'
+        document.getElementById('imgDetails').innerHTML = '<b>设备</b><p>--</p><b>时间</b><p>----年--月--日 --:--:--</p><b>位置（中国大陆不可用）</b><div id="map"></div>'
+        document.getElementsByClassName('imageDetails')[0].style.backgroundColor = '#e9e9e8';
+        document.getElementsByClassName('details-img')[0].src = 'img/info.circle.fill.svg';
+        const tags = await ExifReader.load('https://assets.bananacake.top/photos/109.png');
+        const date = new Date(tags['DateCreated']['value'])
+        document.getElementById('imgDetails').innerHTML = '<b>设备</b><p>' + tags['LensModel']['value'] + '</p><b>时间</b><p>' + date.getFullYear() + '年' + (date.getMonth() + 1) + '月' + date.getDate() + '日 ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '</p><b>位置（中国大陆不可用）</b><div id="map"></div>'
+        if(tags['GPSLatitude'] == undefined){
+            document.getElementById('map').innerHTML = '<p>照片不包含位置信息，或位置信息已被去除</p>';
+        }else{
+            var map = L.map('map', {attributionControl: false}).setView([(tags['GPSLatitude']['description'].substring(tags['GPSLatitude']['description'].length - 1) == 'S' ? '-' : '') + tags['GPSLatitude']['description'].substring(0, tags['GPSLatitude']['description'].length - 1), (tags['GPSLongitude']['description'].substring(tags['GPSLongitude']['description'].length - 1) == 'W' ? '-' : '') + tags['GPSLongitude']['description'].substring(0, tags['GPSLongitude']['description'].length - 1)], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', ).addTo(map);
+            L.marker([(tags['GPSLatitude']['description'].substring(tags['GPSLatitude']['description'].length - 1) == 'S' ? '-' : '') + tags['GPSLatitude']['description'].substring(0, tags['GPSLatitude']['description'].length - 1), (tags['GPSLongitude']['description'].substring(tags['GPSLongitude']['description'].length - 1) == 'W' ? '-' : '') + tags['GPSLongitude']['description'].substring(0, tags['GPSLongitude']['description'].length - 1)]).addTo(map);
+        }
+    }else{
+        document.getElementById('imgDetails').style.cssText = 'width: 0px; height: 0px;';
+        document.getElementById('imgDetails').innerHTML = '';
+        document.getElementsByClassName('imageDetails')[0].style.backgroundColor = '';
+        document.getElementsByClassName('details-img')[0].src = 'img/info.circle.svg';
     }
 }
